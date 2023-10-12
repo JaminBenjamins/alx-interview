fcf5bc1bd79cf167cd9da90577dfc3756de08b55
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-12 16:08:19 +0300

    A function that finds the minimum operations to reach a certain value

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for minimum operations on an input"""
+
+
+def minOperations(n):
+    """
+    Gets the fewest number of operations 
+    to have same number of characters as input
+    """
+    # Output should be at least two characters
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops
